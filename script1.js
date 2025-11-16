5e6005bfab82410e87f5601150a4ae800cdfd698
mersidd00 <mersidd00@gmail.com>
2025-11-16 22:23:33 +0100

    script1.js

--- script1.js
+++ script1.js
@@ -102,7 +102,7 @@
         const formData = new FormData(form);
         const data = Object.fromEntries(formData.entries());
 
-        fetch("https://masafoj7.app.n8n.cloud/webhook/order", {
+        fetch("https://hanouti.onrender.com/webhook/order", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(data),
@@ -327,6 +327,7 @@
 
 
     
+
 
 
 
